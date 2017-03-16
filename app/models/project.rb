5f024a3c1161728a8337e13278a140ef6b678840
Yuriy <yuriy.dobryanskiy@gmail.com>
2017-03-16 19:01:37 +0200

    description not required field anymore

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,5 +6,4 @@ class Project < ApplicationRecord
 
   validates :user_id, presence: true
   validates :title,   presence: true, length: { maximum: 150 }
-  validates :description, presence: true
 end
